3a08290c900c7079c9d01e99cd6eebe15d947186
Eric Fithian <ef2413@columbia.edu>
2022-03-03 13:01:05 -0500

    Updated to be ready for Netlify deploy

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,67 @@
+import { useState } from "react"
+
 function Show(props) {
+    const id = props.match.params.id
+    const people = props.people
+    const person = people.find(p => p._id === id)
+
+    // state for form
+    const [editForm, setEditForm] = useState(person)
+
+    // handleChange function for form
+    const handleChange = event => {
+        console.log(event.target);
+        setEditForm({ ...editForm, [event.target.name]: event.target.value })
+    }
+
+    // handlesubmit for form
+    const handleSubmit = event => {
+        event.preventDefault()
+        props.updatePeople(editForm, person._id)
+        // redirect people back to index
+        props.history.push("/")
+    }
+
+    const removePerson = () => {
+        props.deletePeople(person._id)
+        props.history.push("/")
+    }
+
     return (
-        <h1>This is my Show</h1>
-    );
+        <div className="person">
+            <button id="delete" onClick={removePerson}>
+                DELETE
+            </button>
+            <h1>{person.name}</h1>
+            <h2>{person.title}</h2>
+            <img src={person.image} alt={person.name} />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={editForm.name}
+                    name="name"
+                    placeholder="name"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={editForm.image}
+                    name="image"
+                    placeholder="image URL"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={editForm.title}
+                    name="title"
+                    placeholder="title"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Update Person" />
+            </form>
+            
+        </div>
+    ) 
 }
 
-export default Show;
+export default Show
